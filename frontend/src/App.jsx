54ac9119bb94829a6ef10fc0cdbd0975d182d395
Krishna Chamarthy <chamarthysr@gmail.com>
2024-11-27 00:05:22 +0530

    27/11/24

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='container mx-auto overflow-y-hidden px-8'>
+        <Navbar />
+    </div>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between py-6">
+dd
+    </nav>
+  )
+}
+
+export default Navbar
